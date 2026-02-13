4e2068317a69617f0255832204519f757fc05fa9
Manuel Odendahl <wesen@ruinwesen.com>
2026-02-13 10:30:56 -0500

    engine: add scoped runtime redux state slice

--- packages/engine/src/cards/index.ts
+++ packages/engine/src/cards/index.ts
@@ -1,2 +1,14 @@
 export * from './types';
 export { Sel, Param, Ev, Act, ui, defineCardStack } from './helpers';
+export {
+  ensureCardRuntime,
+  setScopedState,
+  patchScopedState,
+  resetScopedState,
+  hypercardRuntimeReducer,
+  selectScopedState,
+  selectMergedScopedState,
+  type HypercardRuntimeState,
+  type HypercardRuntimeStateSlice,
+  type ScopedLookup,
+} from './runtimeStateSlice';
